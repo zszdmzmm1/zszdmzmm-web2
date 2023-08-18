c4942c7da1767c819133982978a76d1f5303fafd
zszdmzmm <1075534380@qq.com>
2023-08-18 09:53:07 +0800

    图片显示

---
+++ src/main/java/day0817/ResponseImgShow.java
@@ -0,0 +1,33 @@
+package day0817;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+
+
+@WebServlet("/img-show")
+public class ResponseImgShow extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = this.getServletContext().getRealPath("./image/QQ图片20230720103937.jpg");
+        FileInputStream fis = new FileInputStream(url);
+        resp.setContentType("image/png");
+        OutputStream sos = resp.getOutputStream();
+        IOUtils.copy(fis, sos);
+        fis.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
