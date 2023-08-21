4a8c2d82b16ee8be926ffa131aa98ffeb89179b0
zszdmzmm <1075534380@qq.com>
2023-08-21 11:43:18 +0800

    随机抽样页面

---
+++ src/main/java/day0815/RandonResult.java
@@ -0,0 +1,38 @@
+package day0815;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/result")
+public class RandonResult extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("""
+                <!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport"
+                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>随机选择</title>
+                </head>
+                <body>
+                        <div>
+                            <img src="image/""" + req.getAttribute("theOne") + """
+                            "height="150" width="150" alt="选中头像"/>
+                        </div>                            
+                </body>
+                </html>""");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ src/main/java/day0821/RandonChooseServlet.java
@@ -0,0 +1,27 @@
+package day0821;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Random;
+
+@WebServlet("/random-choose")
+public class RandonChooseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] serial = {"Z.png", "H.png"};
+        Random random = new Random();
+        String theOne = serial[random.nextInt(serial.length)];
+        req.setAttribute("theOne", theOne);
+        req.getRequestDispatcher("./result").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
