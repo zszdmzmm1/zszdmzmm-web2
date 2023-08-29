fc6a680ba5a6016c0a88a447492485beab437634
zszdmzmm <1075534380@qq.com>
2023-08-29 15:21:40 +0800

    session

---
+++ src/main/java/day0829/GetSessionServlet.java
@@ -0,0 +1,24 @@
+package day0829;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+@WebServlet("/GetSession")
+public class GetSessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String role = (String)session.getAttribute("role");
+        resp.getWriter().write("role=" + role);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ src/main/java/day0829/SetSessionServlet.java
@@ -0,0 +1,25 @@
+package day0829;
+
+import com.mysql.cj.Session;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+@WebServlet("/SetSession")
+public class SetSessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("role", "admin");
+        resp.getWriter().write("done");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
