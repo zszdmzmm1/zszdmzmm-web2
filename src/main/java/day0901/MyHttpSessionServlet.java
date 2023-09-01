768016f13c8a96f1ffe3210cec56498f4c380cc7
zszdmzmm <1075534380@qq.com>
2023-09-01 17:18:18 +0800

    session监听器

---
+++ src/main/java/day0901/MyHttpSessionServlet.java
@@ -0,0 +1,24 @@
+package day0901;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("/MyHttpSessionServlet")
+public class MyHttpSessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
