2ee13d840f3cc578fc4e19344a2b7a805bba59ba
zszdmzmm <1075534380@qq.com>
2023-09-05 16:28:47 +0800

    Hello SLF4J

---
+++ src/main/java/day0905/HelloLog.java
@@ -0,0 +1,12 @@
+package day0905;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloLog {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HelloLog.class);
+        logger.info("Hello World");
+    }
+}
