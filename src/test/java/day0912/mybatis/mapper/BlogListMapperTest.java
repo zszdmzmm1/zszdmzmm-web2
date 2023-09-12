8ce8086cda0e4211416c4b53b1ea0a92e93cea16
zszdmzmm <1075534380@qq.com>
2023-09-12 14:49:59 +0800

    assertNotNull(...getPublishTime)

--- src/test/java/day0912/mybatis/mapper/BlogListMapperTest.java
+++ src/test/java/day0912/mybatis/mapper/BlogListMapperTest.java
@@ -32,7 +32,14 @@ public class BlogListMapperTest {
         List<BlogList> blogLists = mapper.selectAll();
         System.out.println(blogLists.get(1));
         Assertions.assertTrue(blogLists.size() > 0);
-        Assertions.assertNull(blogLists.get(1).getPublishTime());
+    }
+
+    @Test
+    public void selectAllTest2() {
+        BlogListMapper mapper = sqlSessionFactory.openSession(true).getMapper(BlogListMapper.class);
+        List<BlogList> blogLists = mapper.selectAll();
+        System.out.println(blogLists.get(1));
+        Assertions.assertNotNull(blogLists.get(1).getPublishTime());
     }
 
 }
