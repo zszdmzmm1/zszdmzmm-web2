6e9f16ba4108a2587f04c0d50dccc512c5c60f5a
zszdmzmm <1075534380@qq.com>
2023-09-15 15:10:02 +0800

    simpleUserDao -> NopeUserDao

--- src/main/java/day0915/simple/dao/impl/NopUserDao.java
+++ src/main/java/day0915/simple/dao/impl/NopUserDao.java
@@ -2,14 +2,14 @@ package day0915.simple.dao.impl;
 
 import day0915.simple.dao.UserDao;
 
-public class UserDaoImpl implements UserDao {
+public class NopUserDao implements UserDao {
     @Override
     public void showLogin() {
-        System.out.println("UserDaoImpl.showLogin...");
+        System.out.println("NopUserDao.showLogin");
     }
 
     @Override
     public void login() {
-        System.out.println("UserDaoImpl.login...");
+        System.out.println("NopUserDao.login");
     }
 }
